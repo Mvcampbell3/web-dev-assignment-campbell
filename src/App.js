f5075e0a2d4a96ed23421e9c98916c926f72cfd6
Mvcampbell3 <mvcampbell3@gmail.com>
2020-02-25 18:11:48 -0500

    login and signout user from firebase auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import './App.css'
 import firebase from './firebase';
+import Login from './components/Login';
 
 class App extends Component {
 
   state = {
     list: [],
     nameInput: '',
-    descriptionInput: ''
+    descriptionInput: '',
+    login: true,
+    viewLogin: false,
+    emailInput: '',
+    usernameInput: '',
+    passwordInput: ''
   }
 
 
@@ -31,8 +37,10 @@ class App extends Component {
       console.log(user)
       if (user) {
         // does user stuff
+        this.setState({ viewLogin: false })
       } else {
         // not signed in/up
+        this.setState({ viewLogin: true })
       }
     })
   }
@@ -50,19 +58,58 @@ class App extends Component {
     })
   }
 
-  handleInput(e) {
+  handleInput = (e) => {
     console.log(e.target.value);
     this.setState({
       [e.target.name]: e.target.value
     })
   }
 
+  switchLoginSignup = () => {
+    this.setState({ login: !this.state.login })
+  }
+
+  loginUser = () => {
+    if (this.state.login) {
+      if (this.state.emailInput !== '' && this.state.passwordInput !== '') {
+        firebase.auth().signInWithEmailAndPassword(this.state.emailInput, this.state.passwordInput)
+          .catch(err => {
+            console.log(err)
+          })
+      } else {
+        alert('Must enter email and password to login')
+      }
+    } else {
+
+    }
+  }
+
+  signOutUser = () => {
+    firebase.auth().signOut()
+      .then(() => { console.log('user has signed out') })
+      .catch((err) => { console.log(err) })
+  }
+
   render() {
     return (
       <div className="App">
-        <input type="text" name='nameInput' className='input' placeholder='Enter name' value={this.state.nameInput} onChange={(e) => this.handleInput(e)} />
+        {this.state.viewLogin ?
+          <Login
+            login={this.state.login}
+            password={this.state.passwordInput}
+            username={this.state.usernameInput}
+            email={this.state.emailInput}
+            handleInput={this.handleInput}
+            switchLoginSignup={this.switchLoginSignup}
+            loginUser={this.loginUser}
+          />
+          :
+          <button className='button is-danger' onClick={this.signOutUser}>Sign Out</button>
+        }
+
+        {/* <input type="text" name='nameInput' className='input' placeholder='Enter name' value={this.state.nameInput} onChange={(e) => this.handleInput(e)} />
         <input type="text" name='descriptionInput' className='input' placeholder='Enter description' value={this.state.descriptionInput} onChange={(e) => this.handleInput(e)} />
-        <button className="button is-info" onClick={this.sendItem}>Add Item</button>
+        <button className="button is-info" onClick={this.sendItem}>Add Item</button> */}
       </div>
     );
   }
---
+++ src/components/Login/index.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import './login.css'
+
+const Login = (props) => {
+  return (
+    <div className="flex-container">
+      <div className="login-container">
+        <div className="hero is-info">
+          <div className="hero-body has-text-centered">
+            <h1 className="title">{props.login ? "Login" : "SignUp"}</h1>
+          </div>
+        </div>
+        <div className="form-bottom">
+
+          {props.login ?
+            <div>
+              {/* Login Form */}
+              <input
+                type="text"
+                name='emailInput'
+                value={props.emailInput}
+                placeholder='Enter Email...'
+                className="input" onChange={(e) => props.handleInput(e)}
+              />
+              <input
+                type="password"
+                name='passwordInput'
+                value={props.emailInput}
+                placeholder='Enter Password...'
+                className="input"
+                onChange={(e) => props.handleInput(e)}
+              />
+              <div className="button-holder">
+                <button className="button is-success" onClick={props.loginUser}>Submit</button>
+                <button className="button is-info" onClick={props.switchLoginSignup}>Not Signed Up?</button>
+              </div>
+
+            </div>
+            :
+            <div>
+              {/* Sign Up Form */}
+              <input
+                type="text"
+                name='usernameInput'
+                value={props.usernameInput}
+                placeholder='Enter Email...'
+                className="input" onChange={(e) => props.handleInput(e)}
+              />
+              <input
+                type="text"
+                name='emailInput'
+                value={props.emailInput}
+                placeholder='Enter Email...'
+                className="input" onChange={(e) => props.handleInput(e)}
+              />
+              <input
+                type="password"
+                name='passwordInput'
+                value={props.emailInput}
+                placeholder='Enter Password...'
+                className="input"
+                onChange={(e) => props.handleInput(e)}
+              />
+              <div className="button-holder">
+                <button className="button is-success rightBtn">Submit</button>
+                <button className="button is-info" onClick={props.switchLoginSignup}>Already Signed Up?</button>
+              </div>
+
+            </div>
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
