f5226da602997c1683a4226892688556d226711b
Mvcampbell3 <mvcampbell3@gmail.com>
2020-02-26 22:01:27 -0500

    firebase import corrected from dev sdk

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 
 const config = {
   apiKey: "AIzaSyBOPHhwnl2H7LOdWEaSp5jzcrLRs45aKx8",
